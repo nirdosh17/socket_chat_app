54a9b34659ea833e6bf8320c0886fd57aa940dc1
Nirdosh Gautam <nrdshgtm@gmail.com>
2018-08-04 20:57:12 +0545

    Setup socket server and client.

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
+// initialize new instance of socket passing the http server object
+var io = require('socket.io')(http);
+
 // route handler
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+// listen on connection event for incoming sockets
+io.on('connection', function(socket){
+  console.log('User connected.');
+  socket.on('disconnect', function(){
+    console.log('User disconnected.');
+  });
+});
+
 // boot app and keep listening to the port
 http.listen(3000, function(){
-  console.log('Chat app is running!')
-  console.log('Listening on port 3000..')
+  console.log('Chat app is running!');
+  console.log('Listening on port 3000..');
 });
